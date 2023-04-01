8275c50d36fc74c359e1e0d1b7ecdbfc0f2ce0c9
joao <joao.victormeneses@ufrpe.br>
2023-04-01 15:01:58 -0300

    Terminei

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.awt.Color;
+
 public class Main {
     
 
     public static void main(String[] args) {
         
-        Tela janela = new Tela();
+        Tela janela = new Tela("rsc/83fa6c7b2228591d12cfa3d6d3b5dd41.jpg", Color.WHITE, Color.GREEN);
     }
 }
--- src/Tela.java
+++ src/Tela.java
@@ -1,18 +1,69 @@
 import javax.swing.*;
 import java.awt.*;
-//bibliotecas necessárias para trabalhar com imagens
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
+public class Tela extends JFrame {
+    public Tela(String idgif, Color botaoSimCor, Color botaoNaoCor) {
+        ImageIcon imageIcon = new ImageIcon(idgif);
+        Image image = imageIcon.getImage().getScaledInstance(800, 600, Image.SCALE_SMOOTH);
+        ImageIcon resizedImageIcon = new ImageIcon(image);
+        setContentPane(new JLabel(resizedImageIcon));
+    
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setOpaque(false);
+    
+        JButton botaoSim = new JButton("Sim");
+        botaoSim.setBackground(botaoSimCor);
+        JButton botaoNao = new JButton("Não");
+        botaoNao.setBackground(botaoNaoCor);
+    
+        getContentPane().setLayout(null);
+        getContentPane().add(botaoSim);
+        getContentPane().add(botaoNao);
+    
+        botaoSim.setBounds(200, 400, 100, 40);
+        botaoNao.setBounds(500, 400, 100, 40);
 
-public class Tela extends JFrame{
-//construtor de janela
-    public Tela(){
-        JLabel gif = new JLabel(new ImageIcon("rsc/66286c21e00a4ef6707abb4bfe19dcb3.gif")); //objeto que armazena gif
-        add(gif); //add gif na janela
-        setSize(800,600); //tamanho datela
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
         setUndecorated(false);
         setVisible(true);
+
+        botaoSim.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Random rand = new Random();
+                int x = rand.nextInt(getWidth() - botaoSim.getWidth());
+                int y = rand.nextInt(getHeight() - botaoSim.getHeight());
+                botaoSim.setBounds(x, y, 100, 40);
+    
+                SwingUtilities.updateComponentTreeUI(botaoSim.getParent());
+            }
+        });
+     
+        botaoNao.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getContentPane().remove(botaoSim);
+                getContentPane().remove(botaoNao);
+                getContentPane().revalidate();
+                getContentPane().repaint();
+
+                ImageIcon newImageIcon = new ImageIcon("rsc/Imeng24.jpg");
+                Image newImage = newImageIcon.getImage().getScaledInstance(800, 600, Image.SCALE_SMOOTH);
+                ImageIcon newResizedImageIcon = new ImageIcon(newImage);
+                ((JLabel) getContentPane()).setIcon(newResizedImageIcon);
+                
+            }
+        });
+        
+      
+    
+    
     }
     
 }
